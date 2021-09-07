e9f3c68e7251030ec40488258be6bf345a24dac5
louiscutner <56656319+louiscutner@users.noreply.github.com>
2021-09-07 02:53:24 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        gum_pink: "#FF808F",
+        light_pink: "#FFCCD2",
+        dark_pink: "#D3174A",
+      },
+      backgroundImage: (theme) => ({
+        "home-image-mobile": "url('images/home_bg_mb.jpeg')",
+        "home-image-web": "url('images/home_bg_wb.jpeg')",
+        "about-image-mobile": "url('images/about_bg_mb.png')",
+        "about-image-tablet": "url('images/about_bg_tb.png')",
+        "about-image-web": "url('images/about_bg_wb.png')",
+      }),
+      transitionProperty: {
+        height: "max-height",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
